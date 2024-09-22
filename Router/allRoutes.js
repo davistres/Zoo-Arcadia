e92d508c6b68a25bbb07df4d73e7f21f198f2604
davistres <davistres@yahoo.fr>
2024-09-23 00:25:30 +0200

    Création des pages du site

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,30 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
   new Route("/", "Accueil", "/pages/home.html"),
-  new Route("/", "Diaporama", "/pages/diaporama.html"),
+  new Route("/connexion", "Connexion", "/pages/connexion.html"),
+  new Route("/contact", "Contactez nous", "/pages/contact.html"),
+  new Route(
+    "/habitats",
+    "Tous les habitats",
+    "/pages/habitats.html",
+    "/script/habitat.js"
+  ),
+  new Route("/services", "Tous les services", "/pages/services.html"),
+  new Route(
+    "/savane",
+    "Tous les habitats: SAVANE",
+    "/pages/habitats/savane.html"
+  ),
+  new Route(
+    "/jungle",
+    "Tous les habitats: JUNGLE",
+    "/pages/habitats/jungle.html"
+  ),
+  new Route(
+    "/marais",
+    "Tous les habitats: MARAIS",
+    "/pages/habitats/marais.html"
+  ),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
---
+++ script/habitat.js
@@ -0,0 +1,47 @@
+// Sélectionnez tous les boutons
+const buttons = document.querySelectorAll(".learn-more-btn");
+
+// Ajouter un événement de clic à chaque bouton
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    // Obtenez l'ID de l'habitat associé au bouton cliqué
+    const habitatId = event.target.getAttribute("data-habitat");
+    const habitat = document.getElementById(habitatId);
+
+    // Si l'habitat est déjà ouvert, le fermer
+    if (habitat.classList.contains("active")) {
+      habitat.classList.remove("active");
+    } else {
+      // Fermer tous les autres habitats
+      document.querySelectorAll(".h-habitat-item").forEach((item) => {
+        item.classList.remove("active");
+      });
+
+      // Ouvrir l'habitat sélectionné
+      habitat.classList.add("active");
+    }
+  });
+});
+
+/* Pop up */
+// Sélectionner les éléments
+const openPopupBtn = document.querySelector(".open-popup-btn-lion");
+const popup = document.getElementById("popup");
+const closePopupBtn = document.querySelector(".close-popup-btn");
+
+// Ouvrir le pop-up
+openPopupBtn.addEventListener("click", () => {
+  popup.style.display = "flex"; // Afficher le pop-up
+});
+
+// Fermer le pop-up en cliquant sur le bouton de fermeture
+closePopupBtn.addEventListener("click", () => {
+  popup.style.display = "none"; // Cacher le pop-up
+});
+
+// Fermer le pop-up en cliquant en dehors du contenu
+window.addEventListener("click", (e) => {
+  if (e.target === popup) {
+    popup.style.display = "none"; // Cacher le pop-up
+  }
+});
